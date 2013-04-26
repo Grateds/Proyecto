325c1b26da48123f4a42f2a3f5ff2a9c7448f8d2
Franco Brusatti <franco@mojotech.com>
2013-04-26 02:45:18 -0300

    Rename App to a suitable name

--- src/main/java/com/unrc/app/Inmo.java
+++ src/main/java/com/unrc/app/Inmo.java
@@ -1,11 +1,6 @@
 package com.unrc.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class Inmo {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
---
+++ src/test/java/com/unrc/app/InmoTest.java
@@ -0,0 +1,19 @@
+package com.unrc.app;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class InmoTest {
+
+    @Test
+    public void testTest(){
+        assertEquals(10, 10);
+    }
+
+}

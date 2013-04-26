c6c36ade3f7c5819c92890a8a58f92ed782f6be2
Franco Brusatti <franco@mojotech.com>
2013-04-26 03:52:21 -0300

    Add User model

---
+++ src/main/java/com/unrc/app/models/User.java
@@ -0,0 +1,9 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+public class User extends Model {
+  static{
+      validatePresenceOf("first_name", "last_name");
+  }
+}
---
+++ src/test/java/com/unrc/app/UserSpec.java
@@ -0,0 +1,42 @@
+package com.unrc.app;
+
+import com.unrc.app.models.User;
+
+import org.javalite.activejdbc.Base;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.javalite.test.jspec.JSpec.the;
+
+public class UserSpec{
+
+    @Before
+    public void before(){
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "");
+        Base.openTransaction();
+    }
+
+    @After
+    public void after(){
+        Base.rollbackTransaction();
+        Base.close();
+    }
+
+    @Test
+    public void shouldValidateMandatoryFields(){
+
+        User user = new User();
+
+        //check errors
+        the(user).shouldNotBe("valid");
+        the(user.errors().get("first_name")).shouldBeEqual("value is missing");
+        the(user.errors().get("last_name")).shouldBeEqual("value is missing");
+
+        //set missing values
+        user.set("first_name", "John", "last_name", "Doe");
+
+        //all is good:
+        the(user).shouldBe("valid");
+    }
+}

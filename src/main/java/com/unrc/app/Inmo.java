2f658fe2a4a2fae8c09c73ba50fb70046530f16b
Franco Brusatti <franco@mojotech.com>
2013-04-26 03:53:09 -0300

    Save an User into database

--- src/main/java/com/unrc/app/Inmo.java
+++ src/main/java/com/unrc/app/Inmo.java
@@ -1,8 +1,22 @@
 package com.unrc.app;
 
+import com.unrc.app.models.User;
+
+import org.javalite.activejdbc.Base;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Inmo {
     public static void main( String[] args )
     {
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "");
+
+        User e = new User();
+        e.set("email", "user@email.com");
+        e.set("first_name", "John");
+        e.set("last_name", "Doe");
+        e.saveIt();
+
         System.out.println( "Hello World!" );
     }
 }
--- src/test/java/com/unrc/app/TestSuite.java
+++ src/test/java/com/unrc/app/TestSuite.java
@@ -1,6 +1,5 @@
 package com.unrc.app;
 
-
 import org.junit.ClassRule;
 import org.junit.rules.ExternalResource;
 import org.junit.runner.RunWith;
@@ -10,6 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { InmoTest.class } )
+@Suite.SuiteClasses( { InmoTest.class, UserSpec.class } )
 public class TestSuite {
 }

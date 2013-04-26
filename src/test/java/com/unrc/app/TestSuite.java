1c2a630094b06496df902734f31d2a0581e76dd7
Franco Brusatti <franco@mojotech.com>
2013-04-26 03:06:01 -0300

    Add test suite

---
+++ src/test/java/com/unrc/app/TestSuite.java
@@ -0,0 +1,15 @@
+package com.unrc.app;
+
+
+import org.junit.ClassRule;
+import org.junit.rules.ExternalResource;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { InmoTest.class } )
+public class TestSuite {
+}

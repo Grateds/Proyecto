7aa3bcfcd6978af482c3fddd9aea5b128dd265f2
Vercryger <abuzaid.karim@gmail.com>
2013-05-01 23:08:52 -0300

    Update Inmo.java

--- src/main/java/com/unrc/app/Inmo.java
+++ src/main/java/com/unrc/app/Inmo.java
@@ -1,6 +1,7 @@
 package com.unrc.app;
 
 import com.unrc.app.models.User;
+import com.unrc.app.models.Building;
 
 import org.javalite.activejdbc.Base;
 import org.slf4j.Logger;
@@ -12,11 +13,22 @@ public class Inmo {
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
 
         User e = new User();
+   
         e.set("email", "user@email.com");
         e.set("first_name", "John");
         e.set("last_name", "Doe");
         e.saveIt();
-
+    
+        Building i = new Building();
+   
+        i.set("typ", "land");
+        i.set("id_owners", "1");
+        i.set("adress", "ciudad");
+        i.set("txt", "Gran propiedad");
+        i.set("price", "150000");
+        i.set("v_a", "venta");
+        i.saveIt();
+        
         System.out.println( "Hello World!" );
     }
 }

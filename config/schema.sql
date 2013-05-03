850ea2ddee1ca74e3ca396c24773fc90b225ccac
Vercryger <abuzaid.karim@gmail.com>
2013-05-03 11:11:20 -0300

    Update schema.sql
    Compila perfecto, falta colocar las clausulas de restricciones.

--- config/schema.sql
+++ config/schema.sql
@@ -3,68 +3,65 @@
 
 -- DATABASE: inmoapp_development
 
-CREATE DATABASE inmoapp_development;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ inmoapp_development;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
-    id_users  INT(11) NOT NULL AUTO_INCREMENT,
+    id_user  INT(11) NOT NULL AUTO_INCREMENT,
     email VARCHAR(60),
     first_name VARCHAR(56),
     last_name VARCHAR(56),
-    CONSTRAINT pk_users PRIMARY KEY (id_users)
+  CONSTRAINT pk_users PRIMARY KEY (id_user)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS city; -- Inmobiliarias
-CREATE TABLE city(
-    id_city INT(20) NOT NULL AUTO_INCREMENT;
-    name VARCHAR (20) NOT NULL  
+DROP TABLE IF EXISTS citys; -- Inmobiliarias
+CREATE TABLE citys(
+    id_city INT(20) NOT NULL AUTO_INCREMENT,
+    nam VARCHAR (20) NOT NULL,  
+  CONSTRAINT pk_citys PRIMARY KEY (id_city)
 );
-
+	
 DROP TABLE IF EXISTS realstates; -- Inmobiliarias
 CREATE TABLE realstates(
 	id_realstate INT(20) NOT NULL AUTO_INCREMENT,
-	name VARCHAR(30) NOT NULL,
-	city VARCHAR (30),
-	neighborhood VARCHAR(30) default NULL,
-	street VARCHAR(30) default NULL,
- 	n_street INT (10) default NULL,
-	phone INT(15) default NULL,
+	nam VARCHAR(30),
+	id_city INT(20),
+	neighborhood VARCHAR(30) DEFAULT NULL,
+	street VARCHAR(30) DEFAULT NULL,
+ 	n_street INT (10) DEFAULT NULL,
+	phone INT(15) DEFAULT NULL,
 	email VARCHAR(60),
 	site_web VARCHAR(25),
-	owners INT(20) NOT NULL,   
-        CONSTRAINT pk_realstate PRIMARY KEY (id_realstate),
- 	CONSTRAINT fk_realstate_owners FOREIGN KEY (owners) REFERENCES owners (id_owners),
- 	CONSTRAINT fk_realstate_citys FOREIGN KEY (city) REFERENCES citys (city)
+  CONSTRAINT pk_realstate PRIMARY KEY (id_realstate),
+  CONSTRAINT fk_realstates_citys FOREIGN KEY (id_city) REFERENCES citys (id_city)
 );
 
 DROP TABLE IF EXISTS owners; -- Dueños
 CREATE TABLE owners(
-    id_owners INT(20) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL,
-    city VARCHAR (30),
-    neighborhood VARCHAR(30) default NULL,
-    street VARCHAR(30) default NULL,
-    n_street INT (10) default NULL,
+    id_owner INT(20) NOT NULL AUTO_INCREMENT,
+    nam VARCHAR(20),
+    id_city INT (20),
+    neighborhood VARCHAR(30) DEFAULT NULL,
+    street VARCHAR(30) DEFAULT NULL,
+    n_street INT (10) DEFAULT NULL,
     email VARCHAR(60),
-    realstate INT(20) NOT NULL,
-    CONSTRAINT pk_owners PRIMARY KEY (id_owners),
- 	CONSTRAINT fk_realstate_citys FOREIGN KEY (city) REFERENCES citys (city),
-    CONSTRAINT fk_owners_inmobb FOREIGN KEY (realstate) REFERENCES realstates (id_realstate)
+  CONSTRAINT pk_owners PRIMARY KEY (id_owner),
+  CONSTRAINT fk_owners_citys FOREIGN KEY (id_city) REFERENCES citys (id_city)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS buildings; -- Inmuebles
 CREATE TABLE buildings(
-    id_buildings  INT(11) NOT NULL AUTO_INCREMENT,
-    type ENUM('land','farm','house','garage'),
-    owners INT(20) NOT NULL,
-    city VARCHAR (30),
-    neighborhood VARCHAR(30) default NULL,
-    street VARCHAR(30) default NULL,
-    n_street INT (10) default NULL,
+    id_building INT(11) NOT NULL AUTO_INCREMENT,
+    typ ENUM('land','farm','house','garage'),
+    id_owner INT(20),
+    id_city INT(20),
+    neighborhood VARCHAR(30) DEFAULT NULL,
+    street VARCHAR(30) DEFAULT NULL,
+    n_street INT (10) DEFAULT NULL,
     description VARCHAR(200),
-    price INT(10), --Puede ser FLOAT?
+    price FLOAT(10), 
     operation ENUM('venta','alquiler'),
-    CONSTRAINT pk_buildings PRIMARY KEY (id_buildings),
-    CONSTRAINT fk_realstate_citys FOREIGN KEY (city) REFERENCES citys (city),
-    CONSTRAINT fk_buildings_owners FOREIGN KEY (owners) REFERENCES owners (id_owners)
+  CONSTRAINT pk_buildings PRIMARY KEY (id_building),
+  CONSTRAINT fk_buildings_citys FOREIGN KEY (id_city) REFERENCES citys (id_city),
+  CONSTRAINT fk_buildings_owners FOREIGN KEY (id_owner) REFERENCES owners (id_owner)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;

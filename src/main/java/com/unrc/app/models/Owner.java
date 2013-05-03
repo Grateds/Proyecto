f4aa365d71f14086bfc856c1474bf6c065301cf6
Vercryger <abuzaid.karim@gmail.com>
2013-05-03 14:09:51 -0300

    Update Owner.java

--- src/main/java/com/unrc/app/models/Owner.java
+++ src/main/java/com/unrc/app/models/Owner.java
@@ -4,7 +4,6 @@ import org.javalite.activejdbc.Model;
 
 public class Owner extends Model {
   static{
-       validatePresenceOf("adress", "email");
+       validatePresenceOf("nam", "email");
   }
 }
-

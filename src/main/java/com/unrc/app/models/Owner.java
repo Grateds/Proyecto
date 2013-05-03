57a379819cf39ebc915e0c34bb4f384c20e876a7
Pablo Marconi <pablo.inocram@gmail.com>
2013-05-03 13:32:55 -0300

    change name of Owner model

---
+++ src/main/java/com/unrc/app/models/Owner.java
@@ -0,0 +1,10 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+public class Owner extends Model {
+  static{
+      // validatePresenceOf("adress", "email", "id_realstate");
+  }
+}
+

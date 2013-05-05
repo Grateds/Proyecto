b88dc0a99b810c619351b2b639dacfecaa43c6c2
Dario <astorgadm@gmail.com>
2013-05-04 22:44:04 -0300

    Update Inmo.java

--- src/main/java/com/unrc/app/Inmo.java
+++ src/main/java/com/unrc/app/Inmo.java
@@ -3,6 +3,7 @@ package com.unrc.app;
 import com.unrc.app.models.User;
 import com.unrc.app.models.Building;
 import com.unrc.app.models.Owner;
+import com.unrc.app.models.Realstate;
 
 import org.javalite.activejdbc.Base;
 import org.slf4j.Logger;
@@ -12,7 +13,8 @@ public class Inmo {
     public static void main( String[] args )
     {
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
-
+        
+         /* Usuario */
         User e = new User();
    
         e.set("email", "user@email.com");
@@ -24,12 +26,12 @@ public class Inmo {
         Owner o = new Owner(); 
         
         o.set("first_name", "Raquel");
-        o.set("last_name", "Montre");
+        o.set("last_name", "Zulma");
         o.set("id_city", "1");
         o.set("neighborhood", "Santa Rosa");
         o.set("street", "Sobremonte");
         o.set("n_street", "153");
-        o.set("email", "Raquel@Gmail.com");
+        o.set("email", "raquel@Gmail.com");
         o.saveIt();
     
         /* Segundo crear un inmueble */
@@ -43,9 +45,23 @@ public class Inmo {
         i.set("n_street", "153");
         i.set("description", "Gran propiedad");
         i.set("price", "150000");
-        i.set("operation", "venta");
+        i.set("operation", "venta");   
         i.saveIt();
         
+        /* Crear una inmobiliaria */
+        Realstate r = new Realstate();
+        
+        r.set("id_realstate", "1");
+        r.set("name", "Vienes Raices Imperio"); 
+        r.set("id_city", "1");
+        r.set("neighborhood", "Centro");
+        r.set("street", "Baigorria");
+        r.set("n_street", "1250");
+        r.set("phone", "4689765");
+        r.set("email", "vrimperio@outlook.com");
+        r.set("site_web", "www.vrimperio.com");
+        r.saveIt(); 
+
         System.out.println( "Hello World!" );
     }
 }

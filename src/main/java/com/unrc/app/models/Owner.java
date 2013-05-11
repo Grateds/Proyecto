923b924691133dd05ae5d252147ef43fcdb543fb
Dario <astorgadm@gmail.com>
2013-05-11 20:19:25 -0300

    Update Owner.java

--- src/main/java/com/unrc/app/models/Owner.java
+++ src/main/java/com/unrc/app/models/Owner.java
@@ -4,6 +4,8 @@ import org.javalite.activejdbc.Model;
 
 public class Owner extends Model {
   static{
-       validatePresenceOf("first_name", "last_name", "email");
+       validatePresenceOf("first_name").message ( "Por favor, primer nombre" );
+       validatePresenceOf("last_name").message ( "Por favor, segundo nombre" );
+       validateRegexpOf("email", "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");       
   }
 }

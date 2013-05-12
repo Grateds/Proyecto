cafce55d066ccb237ef98a18be559bcf03f3b4d9
Dario <astorgadm@gmail.com>
2013-05-11 22:04:39 -0300

    Update crudOwner.java

--- src/main/java/com/unrc/app/crudOwner.java
+++ src/main/java/com/unrc/app/crudOwner.java
@@ -18,7 +18,15 @@ public class crudOwner {
 	//
 	// Methods
 	//
-
+	public static void create(String first_name, String last_name,String city_id, String neighborhood, String email){
+            Owner o = new Owner(); 
+            o.set("first_name", first_name).set("last_name",last_name).set("city_id", city_id).set("neighborhood", neighborhood).set("email", email).saveIt();
+        };//end create
+        
+        public static void delete(){     
+        };//end delete
+        
+        public static void update(){};
 
 	//
 	// Accessor methods

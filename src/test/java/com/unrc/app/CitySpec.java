7aec57ccf635c93cab9477ad20e5a5050a14d192
Vercryger <abuzaid.karim@gmail.com>
2013-05-16 11:31:39 -0300

    Test Complete

--- src/test/java/com/unrc/app/CitySpec.java
+++ src/test/java/com/unrc/app/CitySpec.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 
 import com.unrc.app.models.City;
 
-@SuppressWarnings("unused")
 public class CitySpec {
 	@Before
     public void before(){
@@ -19,20 +18,32 @@ public class CitySpec {
 
     @After
     public void after(){
-        Base.rollbackTransaction();
+    	Base.rollbackTransaction();
         Base.close();
     }
 
     @Test
     public void shouldValidateMandatoryFields(){
-/*
+
         City city = new City();
 
         //check errors
         the(city).shouldNotBe("valid");
         the(city.errors().get("name")).shouldBeEqual("value is missing");
 
+        city.set("name","space");
+        
         //all is good:
-        the(city).shouldBe("valid");*/
+        the(city).shouldBe("valid");
+    }
+    
+    @Test
+    public void shouldSaveRecord(){
+    	City city = new City();
+    	
+    	city.set("name", "space").saveIt();
+    	
+        City c = City.findFirst("name = ?", "space");
+        the(city.get("id")).shouldBeEqual(c.get("id"));
     }
 }

27ac0e5918537ab1c98132baa0f2142c074790eb
Vercryger <abuzaid.karim@gmail.com>
2013-05-15 21:54:05 -0300

    Update BuildingSpec

--- src/test/java/com/unrc/app/BuildingSpec.java
+++ src/test/java/com/unrc/app/BuildingSpec.java
@@ -8,8 +8,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.unrc.app.models.Building;
+import com.unrc.app.models.City;
+import com.unrc.app.models.Owner;
 
-@SuppressWarnings("unused")
 public class BuildingSpec {
 	@Before
     public void before(){
@@ -25,20 +26,34 @@ public class BuildingSpec {
 
     @Test
     public void shouldValidateMandatoryFields(){
-/*
-        Building building = new Building();
 
+        Building building = new Building();
+        Owner owner = new Owner();
+        owner.set("first_name","Jose");
+        owner.set("last_name","Perez");
+        owner.set("email","jperez@hotmail.com").saveIt();
+       
+        City city = new City();
+        city.set("name", "space").saveIt();
+        
         //check errors
         the(building).shouldNotBe("valid");
         the(building.errors().get("type")).shouldBeEqual("value is missing");
         the(building.errors().get("owner_id")).shouldBeEqual("value is missing");
         the(building.errors().get("city_id")).shouldBeEqual("value is missing");
-        the(building.errors().get("street")).shouldBeEqual("value is missing");
         the(building.errors().get("neighborhood")).shouldBeEqual("value is missing");
+        the(building.errors().get("street")).shouldBeEqual("value is missing");
         the(building.errors().get("n_street")).shouldBeEqual("value is missing");
+        
+        building.set("type","land");
+        building.set("owner_id",owner.getId());
+        building.set("city_id",city.getId());
+        building.set("neighborhood","Santa Rosa");
+        building.set("street","Sobremonte");
+        building.set("n_street","1789");
         
         //all is good:
         the(building).shouldBe("valid");
-        */
+        
     }
 }

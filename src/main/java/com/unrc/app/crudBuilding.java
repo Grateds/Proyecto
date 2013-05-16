96455ee61eafd42712f5aa05b73d26e29384612f
Vercryger <abuzaid.karim@gmail.com>
2013-05-15 21:53:47 -0300

    Update crudBuilding

--- src/main/java/com/unrc/app/crudBuilding.java
+++ src/main/java/com/unrc/app/crudBuilding.java
@@ -14,7 +14,7 @@ public class crudBuilding {
 	public void create(String type, String owner, String city, String neighborhood, String street, String n_street, String description, String price, String operation){
 		City c = City.findFirst("name = ?", city);
 		Owner o = Owner.findFirst("last_name = ?", owner);
-        	Building b = new Building();
+        Building b = new Building();
 		b.set("type", type);
 		b.set("neighborhood", neighborhood);
 		b.set("street", street);
@@ -23,28 +23,20 @@ public class crudBuilding {
 		b.set("price", price);
 		b.set("operation", operation);
 		b.set("owner_id", o.getId());
-        	b.set("city_id", c.getId());
-        	b.saveIt();
+        b.set("city_id", c.getId());
+        b.saveIt();
 	}//end create
 	
 	/** Pre: building.exist() = true **/
 	/** Pos: Deleted building **/
 	public void delete(String id){   
-    		Building c = Building.findFirst("id = ?", id);
-	        c.deleteCascade();
-    	}//end delete
+    	Building c = Building.findFirst("id = ?", id);
+	    c.deleteCascade();
+    }//end delete
 
 	/** Pre: building.exist() = true **/
 	/** Pos: Updated building **/
 	public void update(String name, String new_name){   
-    	
-    	}//end update
 
-	//
-	// Accessor methods
-	//
-
-	//
-	// Other methods
-	//
+	}
 }

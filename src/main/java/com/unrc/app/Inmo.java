63c58f929ed76e28b3011b624d61cba2934a0f46
Dario <astorgadm@gmail.com>
2013-05-17 00:11:50 -0300

    Update Inmo.java

--- src/main/java/com/unrc/app/Inmo.java
+++ src/main/java/com/unrc/app/Inmo.java
@@ -13,14 +13,13 @@ public class Inmo {
         
 //       crudUser e = new crudUser();
 //       e.create("user@email.com", "John", "Doe");   
-//        
+       
 //       /*Create Owner - Update Owner*/
 //       crudOwner o = new crudOwner();     
 //       o.create("Debora","Meltrozo","2","Santa Rosa","Sobremonte","153","deborame@outlook.com");
-//       o.update("1","Elba","Nanon","","","Sobremonte","153","");   
-                
+//       o.update("1","Elba","Nanon","","","Sobremonte","153","");                  
        
-        /*Create Building - Update Building*/
+//      /*Create Building - Update Building*/
 //      crudBuilding b = new crudBuilding();
 //      b.create("land","2","4","Banda Norte","Marcelo T. Alvear","132","Fea","300.000","venta");
 //      b.update("1", "", "1", "10", "", "Lavalle", "", "", "200000", "");

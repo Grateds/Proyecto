dfcaced3e0625f1cf083ce15acdfc5d046d6fb99
Dario <astorgadm@gmail.com>
2013-05-19 22:40:50 -0300

    Update crudOwner.java

--- src/main/java/com/unrc/app/crudOwner.java
+++ src/main/java/com/unrc/app/crudOwner.java
@@ -11,21 +11,21 @@ public class crudOwner {
 	/** Pos: Created owner **/
 	public void create(String first_name, String last_name, String city_id, String neighborhood, String street, String n_street, String email){
 		Owner o = new Owner();
-        o.set("first_name", first_name);
-        o.set("last_name",last_name);
-        o.set("city_id", city_id);
-        o.set("neighborhood", neighborhood);
-        o.set("street", street);
-        o.set("n_street", n_street);
-        o.set("email", email);
-        o.saveIt();
+        	o.set("first_name", first_name);
+        	o.set("last_name",last_name);
+        	o.set("city_id", city_id);
+        	o.set("neighborhood", neighborhood);
+        	o.set("street", street);
+        	o.set("n_street", n_street);
+        	o.set("email", email);
+        	o.saveIt();
 	}//end create
         
         /** Pre: owner.exist() = true **/
 	/** Pos: Delete Owner **/
    	public void delete(String id){   
    		Owner o = Owner.findFirst("id = ?", id);
-        o.deleteCascade();
+        	o.deleteCascade();
    	}//end delete
         
    	/** Pre: owner.exist() = true **/

67199490a5d6ed22ee3782b0d731408ca5531909
pmarconi <pablo.inocram@gmail.com>
2013-05-24 15:28:33 -0300

    real estate request

--- src/main/java/com/unrc/app/Inmoweb.java
+++ src/main/java/com/unrc/app/Inmoweb.java
@@ -11,6 +11,26 @@ public class Inmoweb {
     public static void main( String[] args ) {
         
     	//http://0.0.0.0:4567/
+        Spark.get(new Route("/"){
+            @Override
+            public Object handle(Request request, Response response) {
+                response.type("text/html");
+
+                // return html
+                return "" +
+                "<DOCTYPE html>" +
+                "<html>" +
+                "  <head>" +
+                        "<title>Inmo Web</title>"+
+                "  </head>" +
+                "  <body>" +
+                "    <h1>InmoWeb</h1>" +
+                "    <a href='users/'>Users</a><br>"+
+                "    <a href='realestates/1'>RealEstates</a>"+
+                "  </body>" +
+                "</html>";
+            }
+        });
     	Spark.get(new Route("/users/:id") {
          @Override
          public Object handle(Request request, Response response) {
@@ -23,6 +43,21 @@ public class Inmoweb {
         	 }else{
         	     response.status(404);
         	     return "User not found";
+        	 }
+        }
+        });
+        Spark.get(new Route("/realestates/:id") {
+         @Override
+         public Object handle(Request request, Response response) {
+             Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
+        	 RealEstate rs = RealEstate.findFirst("id = ?", request.params(":id"));
+        	 Base.close();
+        	 
+        	 if (rs != null ){
+        		 return "Name: "+rs.get("name");
+        	 }else{
+        	     response.status(404);
+        	     return "Real Estate not found";
         	 }
         }
       });

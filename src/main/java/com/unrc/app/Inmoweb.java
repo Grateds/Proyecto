8676bfa4744884f1b28e3e4874268a6372bb9b0d
Vercryger <abuzaid.karim@gmail.com>
2013-05-24 12:08:46 -0300

    Update

--- src/main/java/com/unrc/app/Inmoweb.java
+++ src/main/java/com/unrc/app/Inmoweb.java
@@ -1,22 +1,30 @@
 package com.unrc.app;
+
 import static spark.Spark.*;
 
-import spark.*;
+import org.javalite.activejdbc.Base;
 
-/**
- * Hello world!
- *
- */
-public class Inmoweb 
-{
-    public static void main( String[] args )
-    {
-        //http://0.0.0.0:4567/hello
-        get(new Route("/hello") {
+import spark.*;
+import com.unrc.app.models.*;
+
+public class Inmoweb {
+    public static void main( String[] args ) {
+        
+    	//http://0.0.0.0:4567/
+    	Spark.get(new Route("/users/:id") {
          @Override
          public Object handle(Request request, Response response) {
-            return "Hello World!, this is a test";
-         }
+             Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
+        	 User user = User.findFirst("id = ?", request.params(":id"));
+        	 Base.close();
+        	 
+        	 if (user != null ){
+        		 return "Name: "+user.get("first_name");
+        	 }else{
+        	     response.status(404);
+        	     return "User not found";
+        	 }
+        }
       });
 
     }

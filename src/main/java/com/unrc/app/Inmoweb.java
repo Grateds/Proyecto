72678e870433784f5d6c97de6f3c8a3eca25768a
Vercryger <abuzaid.karim@gmail.com>
2013-06-01 16:49:00 -0300

    Web Update

--- src/main/java/com/unrc/app/Inmoweb.java
+++ src/main/java/com/unrc/app/Inmoweb.java
@@ -1,37 +1,36 @@
 package com.unrc.app;
 
 import java.util.List;
-
 import org.javalite.activejdbc.Base;
-
 import spark.*;
 import com.unrc.app.models.*;
 
 public class Inmoweb {
     public static void main( String[] args ) {
         
-        //http://0.0*.0.0:4567/
         Spark.get(new Route("/"){
             @Override
             public Object handle(Request request, Response response) {
                 response.type("text/html");
                 
-                // return html
-                return "" +
+                return 
                 "<DOCTYPE html>" +
                 "<html>" +
-                "  <head>" +
-                        "<title>Inmo Web</title>"+
-                "  </head>" +
-                "<body background = 'http://img190.imageshack.us/img190/7979/wwwplanillasesimitacion.jpg'>"+
-                "<center>    <h1>Inmobiliaria Web</h1>" +
-                "    <a href='users/2'>Users</a><br>"+
-                "    <a href='owner/1'>Owner</a><br>"+
-                "    <a href='owners/'>ListOwners</a><br>"+
-                "    <a href='realestates/1'>RealEstates</a><br>"+
-                "    <a href='adduser/'>Add User</a></br>"+
-                "    <a href='addowner/'>Add Owner</a></center>"+        
-                "</body>" +
+                "	<head>" +
+                "		<meta http-equiv='Content-Type' content='text/html; charset=ISO-8859-1'>" +
+                "		<title>Inmobiliaro Web</title>"+
+                "		<link rel='shortcut icon' href='http://icons.iconarchive.com/icons/deleket/3d-cartoon-vol3/24/Axialis-Icon-Workshop-Classic-icon.png'>"+
+                "   </head>" +
+                "	<body background = 'http://static.giantbomb.com/uploads/original/3/35099/2183980-fez8.jpg'>"+
+                "		<center>    <h1>Inmobiliario Web</h1>" +
+                "   		<a href='users/2'>Users(Building..)</a><br>"+
+                "    		<a href='owner/1'>Owner(Building..)</a><br>"+
+                "    		<a href='owners/'>ListOwners(Building..)</a><br>"+
+                "    		<a href='realestates/1'>RealEstates(Building..)</a><br>"+
+                "    		<a href='adduser/'>Agregar Usuario</a><br>"+
+                "    		<a href='addowner/'>Agregar Due&ntilde;o</a><br>" +
+                "		</center>"+        
+                "	</body>" +
                 "</html>";
             }
         });
@@ -52,8 +51,8 @@ public class Inmoweb {
     		}
         }); //end User
     	
-         Spark.get(new Route("/owner/:id") {
-    		@Override
+        Spark.get(new Route("/owner/:id") {
+        	@Override
     		public Object handle(Request request, Response response) {
     			Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
     			Owner  owner = Owner.findFirst("id = ?", request.params(":id"));
@@ -66,23 +65,23 @@ public class Inmoweb {
     				return "Owner not found";
     			}
     		}
-    	}); //end Owner
+    	}); 
          
-         Spark.get(new Route("/owners/") {
-     		@Override
+        Spark.get(new Route("/owners/") {
+        	@Override
      		public Object handle(Request request, Response response) {
-                    response.type("text/html");
-                    Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
-     			List<Owner> owners = Owner.findAll();
+        		response.type("text/html");
+                Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
+                List<Owner> owners = Owner.findAll();
      			
-                        String ret = "";
+                String ret = "";
      			for(int i=0; i < owners.size(); i++){
-                            Owner o = owners.get(i);
-                            ret = ret+"<b>First name:</b> "+o.get("first_name")+", <b>Last name:</b> "+o.get("last_name")+", <b>Email:</b> "+o.get("email")+"<br><br>";
-                        }   
+     				Owner o = owners.get(i);
+                    ret = ret+"<b>First name:</b> "+o.get("first_name")+", <b>Last name:</b> "+o.get("last_name")+", <b>Email:</b> "+o.get("email")+"<br><br>";
+                }   
      			Base.close();  			
      			return ret;
-                }
+            }
      	});
          
     	Spark.get(new Route("/realestates/:id") {
@@ -99,60 +98,95 @@ public class Inmoweb {
     				return "Real Estate not found";
     			}
     		}
-    	}); //end Real Estate
+    	});
     	
     	Spark.get(new Route("/adduser/") {
     		@Override
     		public Object handle(Request request, Response response) {
+    		    response.type("text/html");
     			
-                response.type("text/html");
-    			
-    			return ""+
-    			"<form method='POST' action='/adduser/'>"+
-    				"Email:   "+"<input name='email'><P>"+
-    				"First name: <input name='fname'><P>"+
-    				"Last name:  <input name='lname' VALUE='last name'><P>"+
-    			 "<input type='submit' value='Add'>"+
-    			 "<input type='reset' value='Reset'></form>";
+    			return 
+    		    "<DOCTYPE html>" +
+    	        "<html>" +
+    	        "	<head>" +
+    	        "		<title>Agregar Usuario</title>"+
+    	        "		<link rel='shortcut icon' href='http://icons.iconarchive.com/icons/deleket/3d-cartoon-vol3/24/Axialis-Icon-Workshop-Classic-icon.png'>"+
+    	        "   </head>" +
+    	        "	<body background = 'http://static.giantbomb.com/uploads/original/3/35099/2183980-fez8.jpg'>"+
+    	        "   	<center><h1>Formulario</h1>" +    			
+    			"		<form method='POST' action='/adduser/'>"+
+    		    "			<p><input name='fname' placeholder='Nombre'></p>"+
+    			"			<p><input name='lname' placeholder='Apellido'</p>"+
+    			"			<p><input name='email' placeholder='someone@example.com'></p>"+ 
+    			"			<input type='submit' value='Add'>"+
+    			"			<input type='reset' value='Reset'>" +    			
+    			"		</form></center>"+       
+                "	</body>" +
+                "</html>";
     			
     		}
-    	}); //end get adduser
+    	}); 
     	
         Spark.post(new Route("/adduser/") {
     		@Override
     		public Object handle(Request request, Response response) {
-                
+    			response.type("text/html");
     			crudUser u = new crudUser();
     			Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
     			u.create(request.queryParams("email"), request.queryParams("fname"), request.queryParams("lname"));
     			Base.close();
     		
-                return "Usuario registrado exitosamente.!";
-
+                return 
+                "<DOCTYPE html>" +
+            	"<html>" +
+            	"	<head>" +
+            	"		<title>Agregar Usuario</title>"+
+                "		<link rel='shortcut icon' href='http://icons.iconarchive.com/icons/deleket/3d-cartoon-vol3/24/Axialis-Icon-Workshop-Classic-icon.png'>"+
+                "   </head>" +
+                "	<body background = 'http://static.giantbomb.com/uploads/original/3/35099/2183980-fez8.jpg'>"+
+                "		<h1>Usuario registrado exitosamente</h1>" +
+                "		<a href='/'>Inicio</a>" +
+                "   </body>" +
+                "</html>";
     		}
     	}); //end post adduser
         
         Spark.get(new Route("/addowner/") {
     		@Override
     		public Object handle(Request request, Response response) {	
-                    
                 response.type("text/html");
     			
-    			return ""+
-    			"<form method='POST' action='/addowner/'>"+
-
-    				"First Name: <input name='first_name'><P>"+
-    				"Last Name:  <input name='last_name'><P>"+
-                                "City id: <input name='city_id'><P>"+
-                                "Neighborhood: <input name='neighborhood'><P>"+ 
-                                "Street: <input name='street'><P>"+ 
-                                "Number Street: <input name='n_street'><P>"+ 
-                                "Email:   "+"<input name='email'VALUE='someone@example.com'><P>"+ 
-                                
-    			 "<input type='submit' value='Add'>"+
-    			 "<input type='reset' value='Reset'></form>";	
+    			return 
+    			"<DOCTYPE html>" +
+    		    "<html>" +
+    		    "	<head>" +
+    		    "		<title>Agregar Usuario</title>"+
+    		    "		<link rel='shortcut icon' href='http://icons.iconarchive.com/icons/deleket/3d-cartoon-vol3/24/Axialis-Icon-Workshop-Classic-icon.png'>"+
+    		    "   </head>" +
+    		    "	<body background = 'http://static.giantbomb.com/uploads/original/3/35099/2183980-fez8.jpg'>"+
+    		    "   	<center><h1>Formulario</h1>" +     			
+    			"		<form method='POST' action='/addowner/'>"+
+    			"			<p><input name='first_name' placeholder='Nombre'></p>"+
+    			"			<p><input name='last_name' placeholder='Apellido'></p>"+
+                "			<input name='city_id' placeholder='Ciudad'>" +
+                "			<SELECT NAME='selCombo' SIZE=1 onChange='javascript:alert('prueba');'>"+ 
+                "				<OPTION VALUE='link pagina 1'>Seleccionar</OPTION>"+
+                "				<OPTION VALUE='link pagina 2'>Ciudad 1</OPTION>"+
+                "				<OPTION VALUE='link pagina 3'>Ciudad 2</OPTION>"+
+                "				<OPTION VALUE='link pagina 4'>Ciudad 3</OPTION> "+
+                "				<OPTION VALUE='link pagina 4'>..</OPTION> "+
+                "			</SELECT>"+
+                "			<p><input name='neighborhood' placeholder='Barrio'></p>"+ 
+                "			<p><input name='street' placeholder='Calle'>"+ 
+                "			<p><input name='n_street' placeholder='Numero'></p>"+ 
+                "			<p><input name='email' placeholder='someone@example.com'></p>"+ 
+                "			<input type='submit' value='Add'>"+
+    			"			<input type='reset' value='Reset'>" +
+    			"		</form></center>"+
+    			"	</body>" +
+                "</html>";
     		}
-    	}); //end get addowner
+    	}); 
         
         Spark.post(new Route("/addowner/") {
     		@Override
@@ -161,11 +195,10 @@ public class Inmoweb {
     			crudOwner o = new crudOwner();
     			Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
     			o.create(request.queryParams("first_name"), request.queryParams("last_name"), request.queryParams("city_id"),request.queryParams("neighborhood"),request.queryParams("street"),request.queryParams("n_street"),request.queryParams("email"));			
-                        Base.close();
+                Base.close();
     		
                 return "Propietario registrado exitosamente.!";
-
-    		}
-    	}); //end post addowner
+            }
+    	}); 
     }
 }
